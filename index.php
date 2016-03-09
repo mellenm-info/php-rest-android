88cdd14ede2c0fb2e3812a157d79a71135ec7466
mellenm-info <mellenm.info@gmail.com>
2016-03-09 11:10:42 +0100

    ajout d'une vrai structure html

--- index.php
+++ index.php
@@ -1,9 +1,20 @@
-<?php echo 'Hello PHP!' ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Vrai html</title>
+  </head>
+  <body>
+    <h1><?php echo 'Hello PHP!' ?></h1>
 
-caca
+    <q>caca</q>
 
-<?php echo 'Hola les indiens et les allemands' ?>
- <?php echo 'je suis un conflit' ?>
-<div class="seb">
-  bonjour
-</div>
+    <?php echo '<p>Hola les indiens et les allemands</p>' ?>
+     <?php echo '<p>je suis un conflit</p>' ?>
+    <div class="seb">
+      <p>
+        bonjour
+      </p>
+    </div>
+  </body>
+</html>
